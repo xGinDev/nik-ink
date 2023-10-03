d0a71bdf505a43058bd927de46d1604316f614f3
John Correa <jecl2912@gmail.com>
2023-10-02 22:48:24 -0500

    feat: add dark mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,9 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
 import { cookies } from "next/headers";
 import LogoutButton from "../components/LogoutButton";
+import Image from "next/image";
+import Logo from "../public/header/Logo.png";
+import { ModeToggle } from "./DarkMode/Button";
 export const dynamic = "force-dynamic";
 
 export default async function Header() {
@@ -12,19 +15,19 @@ export default async function Header() {
   } = await supabase.auth.getUser();
 
   return (
-    <nav className="w-full flex justify-center">
-      <div className="w-full">
-        {user ? (
-          <div className="flex items-center gap-4">
-            Hey, {user.email}!
-            <LogoutButton />
-          </div>
-        ) : (
-          <Link href="/login" className="">
-            Login
-          </Link>
-        )}
-      </div>
-    </nav>
+    <div className="w-full flex items-center justify-between">
+      <Image src={Logo} alt="Tattoo Logo" width={100} height={100} />
+      {user ? (
+        <div className="flex items-center gap-4">
+          Hey, {user.email}!
+          <LogoutButton />
+        </div>
+      ) : (
+        <Link href="/login" className="">
+          Login
+        </Link>
+      )}
+      <ModeToggle />
+    </div>
   );
 }
