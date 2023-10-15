edb70ca6fb327d33521b78500f64dc9f0df608f8
John Correa <jecl2912@gmail.com>
2023-10-15 15:30:25 -0500

    fix: forgot password

--- components/Header/Login/page.tsx
+++ components/Header/Login/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import Messages from "./messages";
 import { Button } from "@/components/ui/button";
@@ -11,6 +12,7 @@ import {
 
 export function Login() {
   const [resetPass, setResetPass] = useState(false);
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -21,35 +23,56 @@ export function Login() {
           <DialogTitle>Log in or sign up</DialogTitle>
         </DialogHeader>
         <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-          <form
-            className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-            action="/auth/sign-in"
-            method="post"
-          >
-            <label className="text-md" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="rounded-md px-4 py-2 bg-inherit border mb-6"
-              name="email"
-              placeholder="you@example.com"
-              required
-            />
-            <label className="text-md" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="rounded-md px-4 py-2 bg-inherit border mb-6"
-              type="password"
-              name="password"
-              placeholder="••••••••"
-              required
-            />
-            <button className="bg-secondary rounded px-4 py-2 text-white mb-2">
-              Sign In
-            </button>
-            <Messages />
-          </form>
+          {resetPass ? (
+            <form action="">
+              <h1>Forgot passowrd</h1>
+              <button
+                onClick={() => {
+                  setResetPass(!resetPass);
+                }}
+                className="bg-secondary rounded px-4 py-2 text-white mb-2"
+              >
+                Forgot passowrd
+              </button>
+            </form>
+          ) : (
+            <form
+              className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+              action="/auth/sign-in"
+              method="post"
+            >
+              <label className="text-md" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                name="email"
+                placeholder="you@example.com"
+                required
+              />
+              <label className="text-md" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                type="password"
+                name="password"
+                placeholder="••••••••"
+                required
+              />
+              <p
+                onClick={() => {
+                  setResetPass(!resetPass);
+                }}
+              >
+                Forgot password?
+              </p>
+              <button className="bg-secondary rounded px-4 py-2 text-white mb-2">
+                Sign In
+              </button>
+              <Messages />
+            </form>
+          )}
         </div>
       </DialogContent>
     </Dialog>
