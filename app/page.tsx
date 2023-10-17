c26c110c8c0fdb9a9ea54cc4aa05e4f7858cc98d
John Correa <jecl2912@gmail.com>
2023-10-16 19:22:01 -0500

    fix: read table

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export default function Index() {
-  return <div className="w-full flex flex-col items-center"></div>;
+import Home from "@/components/Home/Home";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function Index() {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+
+  const { data: local } = await supabase.from("local").select();
+
+  return (
+    <div className="w-full flex flex-col items-center">
+      <Home />
+      <ul className="my-auto text-foreground">
+        {local?.map((local) => (
+          <div className="">
+            <li key={local.id}>{local.name}</li>
+            <li key={local.id}>{local.description}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 }
